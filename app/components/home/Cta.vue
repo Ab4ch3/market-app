5ef6bd8152ab6930881cc7fdea74c713108b9f12
Miguel abache <miiguel.abache@gmail.com>
2026-08-06 23:39:26 -0400

    fix: database neo

--- app/components/home/Cta.vue
+++ app/components/home/Cta.vue
@@ -1,5 +1,25 @@
+<template>
+  <UPageCTA
+    :title="data?.message"
+    :description="data?.description"
+    orientation="horizontal"
+    :links="links"
+  >
+    <img
+      src="https://picsum.photos/640/728"
+      width="320"
+      height="364"
+      alt="Illustration"
+      class="w-full rounded-lg"
+      loading="lazy"
+    />
+  </UPageCTA>
+</template>
+
 <script setup lang="ts">
 import type { ButtonProps } from "@nuxt/ui";
+
+const { data } = await useFetch("/api/v1/hello-world");
 
 const links = ref<ButtonProps[]>([
   {
@@ -14,21 +34,3 @@ const links = ref<ButtonProps[]>([
   },
 ]);
 </script>
-
-<template>
-  <UPageCTA
-    title="Trusted and supported by our amazing community"
-    description="We've built a strong, lasting partnership. Their trust is our driving force, propelling us towards shared success."
-    orientation="horizontal"
-    :links="links"
-  >
-    <img
-      src="https://picsum.photos/640/728"
-      width="320"
-      height="364"
-      alt="Illustration"
-      class="w-full rounded-lg"
-      loading="lazy"
-    />
-  </UPageCTA>
-</template>
